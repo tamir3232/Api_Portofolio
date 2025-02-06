c84a6145b199066e84f6225b4eff90b46f1e4696
tamir <tamirrasydi@gmail.com>
2025-02-07 00:25:09 +0700

    menambah endpoint controller

--- controller/ProfileController.js
+++ controller/ProfileController.js
@@ -1,34 +1,91 @@
-const { Profile } = require('../database/models/index')
-const getDetailProfile = async (req, res) => {
-    res.send('profile')
-}
+const { Profile, Project } = require('../database/models/index')
 
 const getListProfile = async (req, res) => {
 
     try {
        const getListProfile = await Profile.findAll()
        if(!getListProfile || getListProfile.length === 0){
-           return res.status(404).json({message: 'Data not found'})
+            throw {
+                message: 'Data not found',
+                status: 404 
+            }
        }
 
         return res.status(200).json(getListProfile)       
     }catch (error) {
-        return res.status(500)
+        next(error)
     }
     
 }
 
-const createProfile = async (req, res) => {
-    res.send('profile')
+const getDetailProfile = async (req, res, next) => {
+    try {
+        const getProfile = await Profile.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        if(!getProfile || getProfile.length === 0){
+           throw {
+                message: 'Data not found',
+                status: 404 
+           }
+        }
+
+        const getProject = await Project.findAll({
+            where: {
+                profile_id: getProfile.id
+            }
+        })
+        
+        return res.status(200).json({ message : "Data Berhasil Di dapatkan" , data: {profile: getProfile, projects: getProject}})
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const createProfile = async (req, res,next) => {
+    const { name, greeting, job_desk, name_file, portofolio1_icon, portofolio1_link, portofolio2_icon, portofolio2_link, deskripsi, deskripsi_aboutme, experience, education, profile1_name_file, profile2_name_file, email, phone } = req.body
+    try {
+        const createProfile = await Profile.create({
+            name, greeting, job_desk, name_file, portofolio1_icon, portofolio1_link, portofolio2_icon, portofolio2_link, deskripsi, deskripsi_aboutme, experience, education, profile1_name_file, profile2_name_file, email, phone
+        })
+        return res.status(201).json(createProfile)
+    } catch (error) {
+        next(error)
+    }  
 }
 
 const updateProfile = async (req, res) => {
-    res.send('profile')
+    const { name, greeting, job_desk, name_file, portofolio1_icon, portofolio1_link, portofolio2_icon, portofolio2_link, deskripsi, deskripsi_aboutme, experience, education, profile1_name_file, profile2_name_file, email, phone } = req.body
+    try {
+        const updateProfile = await Profile.update({
+            name, greeting, job_desk, name_file, portofolio1_icon, portofolio1_link, portofolio2_icon, portofolio2_link, deskripsi, deskripsi_aboutme, experience, education, profile1_name_file, profile2_name_file, email, phone
+        },{
+            where: {
+                id: req.params.id
+            }
+        })
+        return res.status(200).json({ message: 'Data Berhasil Di Update'})
+    } catch (error) {
+        next(error)
+    }
 }
 
 const deleteProfile = async (req, res) => {
-    res.send('profile')
+    try {
+        const deleteProfile = await Profile.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        return res.status(200).json({ message: 'Data Berhasil Di Hapus'})
+    } catch (error) {
+        next(error)
+    }
 }
+
 
 module.exports = {
     getDetailProfile,
